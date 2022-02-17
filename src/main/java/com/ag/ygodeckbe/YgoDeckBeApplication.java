76f540a9b1c6b50fe1a0cf38277aa560630dbf3c
Anthony Giang <anthonyhgiang@gmail.com>
2022-02-16 20:28:07 -0800

    Initial commit of Spring Boot project

---
+++ src/main/java/com/ag/ygodeckbe/YgoDeckBeApplication.java
@@ -0,0 +1,13 @@
+package com.ag.ygodeckbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YgoDeckBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YgoDeckBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ag/ygodeckbe/YgoDeckBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ag.ygodeckbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YgoDeckBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
