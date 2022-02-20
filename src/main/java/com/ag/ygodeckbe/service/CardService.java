08d47311bfb88c9b52965c670939521659128df4
Anthony Giang <anthonyhgiang@gmail.com>
2022-02-20 15:15:23 -0800

    Add CardService

---
+++ src/main/java/com/ag/ygodeckbe/service/CardService.java
@@ -0,0 +1,23 @@
+package com.ag.ygodeckbe.service;
+
+import com.ag.ygodeckbe.model.Card;
+import com.ag.ygodeckbe.repository.CardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CardService {
+
+    private final CardRepository cardRepository;
+
+    @Autowired
+    public CardService(CardRepository cardRepository) {
+        this.cardRepository = cardRepository;
+    }
+
+    public List<Card> getAllCards() {
+        return cardRepository.findAll();
+    }
+}
