25897e52d200eccbfcfb95a63901fc0fcd4c05d5
Anthony Giang <anthonyhgiang@gmail.com>
2022-02-20 15:46:42 -0800

    Add HealthController to WebMvcTest annotation

--- src/test/java/com/ag/ygodeckbe/controller/HealthControllerTest.java
+++ src/test/java/com/ag/ygodeckbe/controller/HealthControllerTest.java
@@ -11,7 +11,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
-@WebMvcTest
+@WebMvcTest(controllers = HealthController.class)
 public class HealthControllerTest {
 
     @Autowired
